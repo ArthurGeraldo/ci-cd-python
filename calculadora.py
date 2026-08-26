ef67e90a3b6336065b86b51be8770addb5a3b2fe
ArthurGeraldo <arthurgeraldo718@gmail.com>
2026-08-26 20:28:01 -0300

    comit com erro

--- calculadora.py
+++ calculadora.py
@@ -7,7 +7,7 @@ def subtrair(a, b):
 def multiplicar(a, b): 
     return a * b 
  
-def dividir(a, b):
-    if b == 0:
-        raise ValueError("Divisão por zero não permitida")
-    return a / b
+def dividir(a, b): 
+    if b == 0: 
+        raise ValueError("Divisão por zero não permitida") 
+    return a / b 
